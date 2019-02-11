5b0b5c400482ff31f794ea21c5a32c1ad6e45627
Caio <frias.caio@yahoo.com.br>
2019-02-11 21:50:56 -0200

    This is a new commit for what I originally planned to be amended

---
+++ app/src/main/java/meunovoapp/caio/com/listadetarefas/db/TaskDBHelper.java
@@ -0,0 +1,26 @@
+package meunovoapp.caio.com.listadetarefas.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TaskDBHelper  extends SQLiteOpenHelper{
+
+
+    public TaskDBHelper(Context context) {
+        super(context,TaskContract.DB_NAME, null, TaskContract.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        String sqlQuery = String.format("CREATE TABLE %s(_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "%s TEXT,%s TEXT)",TaskContract.TABLE,TaskContract.Columns.TAREFA,TaskContract.Columns.PRAZO );
+        sqLiteDatabase.execSQL(sqlQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase , int oldVersion, int newVersion) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TaskContract.TABLE);
+    }
+}
